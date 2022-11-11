318cfc72859309dec3c04fe47de23b0171b47c23
cavishek39 <cavishek39@gmail.com>
2022-11-11 20:22:20 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import Contacts from './pages/Contacts'
+import Cart from './pages/Cart'
+import SingleProduct from './pages/SingleProduct'
+import ErrorPages from './pages/ErrorPages'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider>
+      <ColorModeProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/contact' element={<Contacts />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/singleproduct/:id' element={<SingleProduct />} />
+            <Route path='*' element={<ErrorPages />} />
+          </Routes>
+        </Router>
+      </ColorModeProvider>
+    </ChakraProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/ErrorPages.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ErrorPages = () => {
+  return <div>ErrorPages</div>
+}
+
+export default ErrorPages
